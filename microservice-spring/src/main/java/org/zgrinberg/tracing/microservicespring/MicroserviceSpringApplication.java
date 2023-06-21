9b7efec12a1dedd41c0518a81c85ff3ceea4fc27
Zvi Grinberg <zgrinber@redhat.com>
2023-06-21 15:33:36 +0300

    modify and extend the outline skeleton of the project
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

---
+++ microservice-spring/src/main/java/org/zgrinberg/tracing/microservicespring/MicroserviceSpringApplication.java
@@ -0,0 +1,13 @@
+package org.zgrinberg.tracing.microservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSpringApplication.class, args);
+	}
+
+}
---
+++ microservice-spring/src/test/java/org/zgrinberg/tracing/microservicespring/MicroserviceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zgrinberg.tracing.microservicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
